79c9ed3f3b49ae9e20aa1ff49fb844f68b26cfde
Farbod <farbodsaghafimail@gmail.com>
2022-06-30 11:54:13 +0430

    ForumListModel created.

---
+++ src/Models/ForumListModel.java
@@ -0,0 +1,15 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class ForumListModel {
+    private ArrayList<ForumModel> forums = null;
+
+    public ArrayList<ForumModel> getForums() {
+        return forums;
+    }
+
+    public void setForums(ArrayList<ForumModel> forums) {
+        this.forums = forums;
+    }
+}
